993c54944445fb81740e9f88a0aacc856622d9bb
Cynth <cynthia.enciso28@gmail.com>
2021-01-26 18:20:40 -0600

    Added getTickets lambda and service function

--- frontend-native/src/Components/Screens/TicketForm.tsx
+++ frontend-native/src/Components/Screens/TicketForm.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 import styles from '../../../gobal-styles';
+import zooService from '../../../services/zoo.service';
 import { Button } from '../Button';
 import { Form } from '../Form';
 import { Info } from '../Info';
 import { Title } from '../Title';
 
 function TicketForm() {
+
+  async function testing() {
+    const tickets = await zooService.getTickets();
+    console.log(tickets);
+    alert('hello');
+  }
+
   return (
-    <View style={styles.viewContainer}>
+    <View>
       <Title title='PURCHASE A TICKET' />
       <View style={styles.purchaseTicketView}>
         <View style={{ alignSelf: 'center' }}>
@@ -19,12 +27,11 @@ function TicketForm() {
                 <Form name='Age'/>
                 <Form name='Date'/>
                 <Form name='Time'/> */}
-        <Form name='Number of Children ($X ea.)  ' />
-        <Form name='Number of Adults ($X ea.)  ' />
-        <Form name='Number of Seniors ($X ea.)  ' />
+        <Form name='Number of Children ($X ea.)' />
+        <Form name='Number of Adults ($X ea.)' />
+        <Form name='Number of Seniors ($X ea.)' />
         <Form name='Event/Exhibit' />
-        <Text> </Text>
-        <Button>PURCHASE</Button>
+        <Button onPress={testing()}>PURCHASE</Button>
       </View>
     </View>
   );
