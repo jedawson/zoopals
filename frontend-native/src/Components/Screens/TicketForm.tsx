6bf513d760c56a33af02c5db1d1ae41511878961
Cynth <cynthia.enciso28@gmail.com>
2021-01-26 21:36:23 -0600

    View tickets available functionality completed

--- frontend-native/src/Components/Screens/TicketForm.tsx
+++ frontend-native/src/Components/Screens/TicketForm.tsx
@@ -1,37 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, Alert } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import styles from '../../../gobal-styles';
+import { Ticket } from '../../../models/ticket';
 import zooService from '../../../services/zoo.service';
 import { Button } from '../Button';
 import { Form } from '../Form';
 import { Info } from '../Info';
 import { Title } from '../Title';
 
+/**
+ * TicketForm displays the available tickets for purchase.
+ * This function also allows you to purchase multiple tickets.
+ */
 function TicketForm() {
 
-  async function testing() {
-    const tickets = await zooService.getTickets();
-    console.log(tickets);
-    alert('hello');
+  // create ticket state
+  const [tickets, setTickets] = useState([[]]);
+
+  useEffect( () => {
+
+    // get tickets from the database after each render
+    async function getTickets() {
+      const tickets = await zooService.getTickets();
+      setTickets(tickets);
+    }
+    getTickets();
+  }, []);
+  
+  // check if the tickets returned are accurate
+  console.log('Tickets returned from db: ', tickets);
+  console.log(typeof tickets);
+  console.log(Object.values(tickets));
+
+  // create a ticket interface for rendering each ticket
+  interface ticket {
+    price: number,
+    tickettype: string,
+    specialeventname: string,
+    datetime: string
   }
+
+  // variable called that renders an individual ticket
+  const renderTicket = ( {item}: {item: ticket}) => (
+    <View>
+      <Text>Price: {item.price}</Text>
+      <Text>Type: {item.tickettype}</Text>
+      <Text>Event: {item.specialeventname}</Text>
+      <Text>Date/Time: {item.datetime}</Text>
+    </View>
+  );
 
   return (
     <View>
       <Title title='PURCHASE A TICKET' />
       <View style={styles.purchaseTicketView}>
+
+        {/* list of tickets */}
+        <FlatList
+        data={tickets}
+        renderItem={renderTicket}
+        />
+
         <View style={{ alignSelf: 'center' }}>
           <Info name='Total'>$XX.XX</Info>
         </View>
-        {/* We don't need the commented fields below, they need to be added to a registration screen */}
-        {/* <Form name='Name'/>
-                <Form name='Age'/>
-                <Form name='Date'/>
-                <Form name='Time'/> */}
-        <Form name='Number of Children ($X ea.)' />
-        <Form name='Number of Adults ($X ea.)' />
-        <Form name='Number of Seniors ($X ea.)' />
-        <Form name='Event/Exhibit' />
-        <Button onPress={testing()}>PURCHASE</Button>
+
+        <Button>PURCHASE</Button>
+
       </View>
     </View>
   );
