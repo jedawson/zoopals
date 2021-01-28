7013bd51f6b145399da601b95e8e2f9f2fc5aef9
Cynth <cynthia.enciso28@gmail.com>
2021-01-27 20:46:52 -0600

    Added styles to TicketForm component

--- frontend-native/src/Components/Screens/TicketForm.tsx
+++ frontend-native/src/Components/Screens/TicketForm.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, Alert } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { View, Text, SafeAreaView} from 'react-native';
+import { FlatList, ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../../../gobal-styles';
-import { Ticket } from '../../../models/ticket';
 import zooService from '../../../services/zoo.service';
 import { Button } from '../Button';
-import { Form } from '../Form';
 import { Info } from '../Info';
 import { Title } from '../Title';
+import { Dimensions, StyleSheet } from 'react-native';
 
 /**
  * TicketForm displays the available tickets for purchase.
@@ -41,36 +40,88 @@ function TicketForm() {
     datetime: string
   }
 
-  // variable called that renders an individual ticket
-  const renderTicket = ( {item}: {item: ticket}) => (
-    <View>
-      <Text>Price: {item.price}</Text>
-      <Text>Type: {item.tickettype}</Text>
-      <Text>Event: {item.specialeventname}</Text>
-      <Text>Date/Time: {item.datetime}</Text>
-    </View>
-  );
-
   return (
-    <View>
-      <Title title='PURCHASE A TICKET' />
-      <View style={styles.purchaseTicketView}>
+    <View style={styles.purchaseTicketView}>
+      
+      <ScrollView>
+        <Title title='PURCHASE A TICKET' />
 
-        {/* list of tickets */}
-        <FlatList
-        data={tickets}
-        renderItem={renderTicket}
-        />
+        {/* Table header */}
+        <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            marginLeft: 10,
+            marginRight: 10,
+            backgroundColor: '#2C7B56',
+            padding: 5
+          }}>
+            <Text style={[flexStyle.flexPriority, {color: '#FFFFFF'}]}>Price</Text>
+            <Text style={[flexStyle.flexPriority, {color: '#FFFFFF'}]}>Ticket Type</Text>
+            <Text style={{ flex: 1.5,alignSelf: 'center',justifyContent: 'center', color: '#FFFFFF'}}>Event</Text>
+            <Text style={{ flex: 1.5,alignSelf: 'center',justifyContent: 'center', color: '#FFFFFF'}}>Date/Time</Text>
+            <Text style={[flexStyle.flexPriority, {color: '#FFFFFF'}]}>Quantity</Text>
+          </View>
 
-        <View style={{ alignSelf: 'center' }}>
+          {/* list of tickets */}
+          <FlatList
+            data={tickets}
+            renderItem={({item}: {item: ticket}) => (
+              <View style={flexStyle.horizontalFlexContainer}>
+                <Text style={[flexStyle.flexPriority]}>${item.price}</Text>
+                <Text style={[flexStyle.flexPriority]}>{item.tickettype}</Text>
+                <Text style={{ flex: 1.5,alignSelf: 'center',justifyContent: 'center'}}>{item.specialeventname}</Text>
+                <Text style={{flex: 1.5,alignSelf: 'center',justifyContent: 'center' }}>{item.datetime}</Text>
+                <View style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  alignSelf: 'center',
+                  backgroundColor: '#FFF',
+                  padding: 5
+                  
+                }}>
+                  <TouchableOpacity style={[flexStyle.button, flexStyle.flexPriority]}><Text>-</Text></TouchableOpacity>
+                  <Text> 0 </Text>
+                  <TouchableOpacity style={[flexStyle.button, flexStyle.flexPriority]}><Text>+</Text></TouchableOpacity>
+                </View>
+              </View>)}
+            keyExtractor={ (item, index) => item.tickettype + index.toString()}
+          />
+          <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', paddingBottom: 10, marginTop: 30}}>
           <Info name='Total'>$XX.XX</Info>
-        </View>
-
-        <Button>PURCHASE</Button>
-
+          <Button>PURCHASE</Button>
       </View>
+        
+      </ScrollView>
     </View>
   );
 }
 
+
+const flexStyle = StyleSheet.create(
+ {
+   horizontalFlexContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: '#FFF',
+    marginBottom: 5,
+    padding: 5
+   },
+  flexPriority: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#67a2e5',
+    padding: 5,
+    borderRadius: 5
+  },
+  title: {
+    fontWeight: 'bold'
+  }
+ }
+);
 export { TicketForm };
