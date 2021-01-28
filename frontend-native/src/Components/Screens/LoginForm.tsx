94e58722c2a93567a246f6edec7829c2eae2c9ea
Jonathan Dawson <dawsonjed@gmail.com>
2021-01-28 12:44:36 -0600

    start loginform changing

--- frontend-native/src/Components/Screens/LoginForm.tsx
+++ frontend-native/src/Components/Screens/LoginForm.tsx
@@ -1,19 +1,38 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser, loginAction } from '../../../store/action';
 import styles from '../../../gobal-styles';
 import { Button } from '../Button';
 import { Form } from '../Form';
 import { Title } from '../Title';
 
 function LoginForm() {
+  async function loginUser() {
+
+  }
+
   return (
     <View style={styles.viewContainer}>
       <Title title='WELCOME!' />
       <View style={styles.loginView}>
-        <Form name='Username' />
-        <Form name='Password' />
-        <Button>LOGIN</Button>
+        <Text>Username: </Text>
+          <TextInput
+            onChangeText={(value) =>
+                dispatch(loginAction({ ...user, name: value }))
+            }
+            value={user.name}
+          />
+        <Text>Password: </Text>
+          <TextInput
+            secureTextEntry={true}
+            onChangeText={(value) =>
+                dispatch(loginAction({ ...user, password: value }))
+            }
+            value={user.password}
+          />
+        <Button onPress={loginUser()}>LOGIN</Button>
       </View>
     </View>
   );
