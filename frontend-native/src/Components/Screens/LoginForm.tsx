41f3c0ad02d5f082ae534bce9d2f5da0192bd657
Jonathan Dawson <dawsonjed@gmail.com>
2021-01-30 17:34:33 -0600

    clean up unused imports, dead code, and some console logs

--- frontend-native/src/Components/Screens/LoginForm.tsx
+++ frontend-native/src/Components/Screens/LoginForm.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity, Button, TextInput } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from '../../../global-styles';
 import { getUser, loginAction } from '../../../store/action';
 import { UserState } from '../../../store/store';
-// import { Button } from '../Button';
-import { Form } from '../Form';
 import { Title } from '../Title';
 import userService from '../../../services/user.service';
 import { User } from '../../../models/user';
@@ -17,14 +15,10 @@ interface LoginProp {
 function LoginForm({ navigation }: LoginProp) {
   const selectUser = (state: UserState) => state.loginUser;
   const user = useSelector(selectUser);
-  //user.username = 'placeholder username'
   const dispatch = useDispatch();
+
   //handles the login button
   function submitForm() {
-    // Since no login at the moment, we can change this user to
-    // simulate a user being got from the database
-    //user = { ...user, role: 'Customer', zookeepers: [] };
-    //dispatch(getUser(user));
     console.log(`User: ${JSON.stringify(user)}`);
 
     userService.signIn(user.username, user.password).then((user) => {
@@ -48,11 +42,9 @@ function LoginForm({ navigation }: LoginProp) {
         <Text>Username: </Text>
         <TextInput
           style={styles.inputBox}
-          onChangeText={(value) => {
-            console.log(value);
-            dispatch(loginAction({ ...user, username: value }));
-            console.log(user.username);
-          }}
+          onChangeText={(value) => 
+            dispatch(loginAction({ ...user, username: value }))
+          }
           value={user.username}
         />
         <Text>Password: </Text>
