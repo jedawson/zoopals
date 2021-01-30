340c887acc35df69d3f31e7352afb7c4d22675b9
kathrync23 <kathrynclark16@augustana.edu>
2021-01-30 16:44:33 -0600

    fixed the login so that the user information from the database gets spread correctly and refactored the navigation.

--- frontend-native/src/Components/Screens/LoginForm.tsx
+++ frontend-native/src/Components/Screens/LoginForm.tsx
@@ -30,61 +30,44 @@ function LoginForm({ navigation }: LoginProp) {
     userService.signIn(user.username, user.password).then((user) => {
       console.log(user);
       if (user) {
-        let newUser = new User();
-        newUser.username = user.username;
-        newUser.password = user.password;
-        newUser.role = user.role;
+        let newUser = { ...user };
         dispatch(getUser(newUser));
         // Checks the user role to determine which Screen to go to.
-        if (user.role === 'Zookeeper') {
-          navigation.navigate('Zookeeper', { screen: 'Home' });
-        } else if (user.role === 'Manager') {
-          navigation.navigate('Manager', { screen: 'Home' });
-        } else {
-          navigation.navigate('Customer', { screen: 'Home' });
-        }
+        navigation.navigate(`${newUser.role}`, { screen: 'Home' });
       } else {
-        alert('Username and/or password is incorrect')
-        dispatch(getUser(new User()))
+        alert('Username and/or password is incorrect');
+        dispatch(getUser(new User()));
       }
-      
-  });
-    
+    });
   }
-
-  // Needed for when we get the login in functionality
-  // useEffect(() => {
-  //   dispatch;
-  // }, [user]);
 
   return (
     <View style={styles.viewContainer}>
       <Title title='WELCOME!' />
       <View style={styles.loginView}>
-      <Text>Username: </Text>
-      <TextInput 
-        style={styles.inputBox}
-          onChangeText={(value) =>
-            {console.log(value)
-              dispatch(loginAction({ ...user, username: value }))
-              console.log(user.username)
-            }
-          }
+        <Text>Username: </Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={(value) => {
+            console.log(value);
+            dispatch(loginAction({ ...user, username: value }));
+            console.log(user.username);
+          }}
           value={user.username}
-      />
-      <Text>Password: </Text>
-      <TextInput
-        style={styles.inputBox}
+        />
+        <Text>Password: </Text>
+        <TextInput
+          style={styles.inputBox}
           secureTextEntry={true}
           onChangeText={(value) =>
-              dispatch(loginAction({ ...user, password: value }))
+            dispatch(loginAction({ ...user, password: value }))
           }
           value={user.password}
-      />
-      <TouchableOpacity style={styles.button}>
-        <Button onPress={submitForm} title='Login' />
-      </TouchableOpacity>
-      <Text></Text>
+        />
+        <TouchableOpacity style={styles.button}>
+          <Button onPress={submitForm} title='Login' />
+        </TouchableOpacity>
+        <Text></Text>
       </View>
     </View>
   );
