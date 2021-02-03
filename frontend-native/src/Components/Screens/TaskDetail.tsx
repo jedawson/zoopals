edc180bc52a523b07689c578d80d8ed019bd8462
kathrync23 <kathrynclark16@augustana.edu>
2021-02-03 09:53:52 -0600

    added the lambda function for updating Zookeepers. Mostly have the Manager able to add tasks to the Zookeeper

--- frontend-native/src/Components/Screens/TaskDetail.tsx
+++ frontend-native/src/Components/Screens/TaskDetail.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Button, View } from 'react-native';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from '../../../global-styles';
+import { tasks } from '../../../models/animal';
 import { Zookeeper } from '../../../models/user';
+import userService from '../../../services/user.service';
+import { GetZookeeper } from '../../../store/action';
 import { ZookeeperState } from '../../../store/store';
 import { Info } from '../Info';
 import { Title } from '../Title';
 
-interface TaskDetailProps {
+interface AllTaskProps {
   data: Zookeeper;
 }
 
-function TaskDetail({ data }: TaskDetailProps) {
+function AllTaskScreen({ data }: AllTaskProps) {
   const zookeeper = useSelector((state: ZookeeperState) => state.zookeeper);
   console.log('Taskdetail zookeeper: ' + JSON.stringify(zookeeper));
 
@@ -20,11 +23,65 @@ function TaskDetail({ data }: TaskDetailProps) {
     <View style={styles.ticketView}>
       <ScrollView>
         <Title title={zookeeper.username} />
-        <Info> Current Tasks: {zookeeper.tasks}</Info>
+        <Info name='Current Tasks' />
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => (
+            <TaskDetail data={item} zookeeper={zookeeper} />
+          )}
+          keyExtractor={(item) => item}
+        />
         <Title title='Available Tasks' />
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => (
+            <AddTaskDetail data={item} zookeeper={zookeeper} />
+          )}
+          keyExtractor={(item) => item}
+        />
       </ScrollView>
     </View>
   );
 }
 
-export default TaskDetail;
+export default AllTaskScreen;
+
+interface AddTaskDetailProps {
+  data: string;
+  zookeeper: Zookeeper;
+}
+function AddTaskDetail({ data, zookeeper }: AddTaskDetailProps) {
+  const dispatch = useDispatch();
+  function handleAddTask() {
+    console.log('button clicked');
+    zookeeper.tasks.push(data);
+    // userService.updateZookeeper(zookeeper);
+    console.log(zookeeper.tasks);
+    dispatch(GetZookeeper(zookeeper));
+    console.log('boolean: ' + zookeeper.tasks.includes(data));
+  }
+
+  return (
+    <View>
+      {zookeeper.tasks.includes(data) || (
+        <Info>
+          {data + '\t'}
+          <Button onPress={handleAddTask} title='Add Task'>
+            Add Task
+          </Button>
+        </Info>
+      )}
+    </View>
+  );
+}
+
+interface TaskDetailProps {
+  data: string;
+  zookeeper: Zookeeper;
+}
+
+function TaskDetail({ data, zookeeper }: TaskDetailProps) {
+  return (
+    <View>{zookeeper.tasks.includes(data) && <Info>{data + '\t'}</Info>}</View>
+  );
+}
