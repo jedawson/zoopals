f8424d4d77742a95a1b0db803473d74f0e9e1015
Jonathan Dawson <dawsonjed@gmail.com>
2021-02-03 09:48:35 -0600

    get the tasks from user state

--- frontend-native/src/Components/Screens/ZookeeperHome.tsx
+++ frontend-native/src/Components/Screens/ZookeeperHome.tsx
@@ -4,7 +4,7 @@ import { View, StyleSheet, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import styles from '../../../global-styles';
 import { Screens } from '../../../router/router.component';
-import { ZooNameState } from '../../../store/store';
+import { UserState, ZooNameState } from '../../../store/store';
 import { Title } from '../Title';
 
 interface ZookeeperProps {
@@ -16,12 +16,14 @@ function ZookeeperHome(props: ZookeeperProps) {
   console.log(`Zookeeper Home Props: ${JSON.stringify(props)}`);
 
   const [isSelected, setSelection] = useState(false);
-  const user = useSelector((state: ZooNameState) => state.user);
-
+  //const user = useSelector((state: ZooNameState) => state.user);
+  const selectUser = (state: UserState) => state.user;
+  const user = useSelector(selectUser);
+  console.log(JSON.stringify(user))
   return (
     <View style={styles.viewContainer}>
       <Title title='MY TASKS' />
-      <Text>Tasks here</Text>
+      <Text>{user.tasks}</Text>
       <View style={styles.myTasksView}>
         <Text> insert checkboxes here?</Text>
         <Text>{JSON.stringify(props)}</Text>
