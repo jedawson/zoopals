0d2fc280d13792be88e87fc321bb5c63e2fb07a6
Jonathan Dawson <dawsonjed@gmail.com>
2021-02-05 10:08:40 -0600

    make the food item update when clicking on the button

--- frontend-native/src/Components/Screens/ZookeeperHome.tsx
+++ frontend-native/src/Components/Screens/ZookeeperHome.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import { useDispatch, useSelector } from 'react-redux';
-import styles from '../../../global-styles';
-import { Zookeeper } from '../../../models/user';
-import userService from '../../../services/user.service';
-import { getUser } from '../../../store/action';
-import { UserState } from '../../../store/store';
+import React, { useEffect, useState } from "react";
+import { Button, View, Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "../../../global-styles";
+import { Zookeeper } from "../../../models/user";
+import userService from "../../../services/user.service";
+import zooService from "../../../services/zoo.service";
+import { getUser } from "../../../store/action";
+import { UserState } from "../../../store/store";
 
-import { Title } from '../Title';
+import { Title } from "../Title";
 
 interface ZookeeperProps {
   data: any;
@@ -19,23 +20,65 @@ function ZookeeperHome(props: ZookeeperProps) {
   const selectUser = (state: UserState) => state.user;
   const user: Zookeeper = useSelector(selectUser);
   const dispatch = useDispatch();
+  // create types needed
+  interface animalFood {
+    itemid: 0;
+    foodname: "";
+    price: 0;
+    stock: 0;
+  }
+
+  let animalFoodArray: animalFood[] = [];
+
+  // create state needed
+  const [animalFood, setAnimalFood] = useState(animalFoodArray);
+
+  useEffect(() => {
+    // get animal food from db after each render
+    async function getAnimalFood() {
+      const animalFoodReturned = await zooService.getAnimalFood();
+      setAnimalFood(animalFoodReturned);
+    }
+    getAnimalFood();
+  }, []);
 
   function removeItem(task: string) {
     let newUser = { ...user };
     newUser.tasks = newUser.tasks.filter((taskItem: string) => {
       return taskItem != task;
     });
+    if (task == "Feed Animals") {
+      console.log(JSON.stringify(newUser));
+      // animalDiets is an array of diet strings like ['Meat', 'Meat', 'Meat']
+      let animalDiets = newUser.exhibits
+        .map((exhibit) => {
+          return exhibit.animals.map((animal) => {
+            return animal.diet;
+          });
+        })
+        .flat();
+      console.log(animalDiets);
+      animalDiets.forEach((foodString) => {
+        animalFood.forEach((item) => {
+          if (foodString == item.foodname && item.stock > 0) {
+            item.stock--;
+            let parameter = item.itemid + "," + item.stock;
+            zooService.updateAnimalFood(parameter);
+          }
+        });
+      });
+    }
     userService.updateZookeeper(newUser).then((data) => {
       if (data) {
         dispatch(getUser(newUser));
       } else {
-        alert('Did not update the database with your finished task. Try again');
+        alert("Did not update the database with your finished task. Try again");
       }
     });
   }
   return (
     <View style={styles.viewContainer}>
-      <Title title='MY TASKS' />
+      <Title title="MY TASKS" />
       <Text></Text>
       <View style={styles.myTasksView}>
         <FlatList
@@ -48,12 +91,13 @@ function ZookeeperHome(props: ZookeeperProps) {
                   onPress={() => {
                     removeItem(item);
                   }}
-                  title='Finished'
+                  title="Finished"
                 />
               </>
             );
           }}
-          keyExtractor={(item, index) => item + index.toString()}></FlatList>
+          keyExtractor={(item, index) => item + index.toString()}
+        ></FlatList>
         <Text>{JSON.stringify(props)}</Text>
       </View>
     </View>
