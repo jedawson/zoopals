829d360c4bcb98c5684ffbdeb1caa073e81bc278
kathrync23 <kathrynclark16@augustana.edu>
2021-02-15 10:01:24 -0600

    removed a console log

--- frontend-native/src/Components/Screens/Staff.tsx
+++ frontend-native/src/Components/Screens/Staff.tsx
@@ -1,29 +1,28 @@
-import React from "react";
-import { View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { useSelector } from "react-redux";
-import styles from "../../../global-styles";
-import { ZookeeperState } from "../../../store/store";
-import { Title } from "../Title";
-import { ZookeeperDetail } from "./ZookeeperDetail";
+import React from 'react';
+import { View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+import styles from '../../../global-styles';
+import { ZookeeperState } from '../../../store/store';
+import { Title } from '../Title';
+import { ZookeeperDetail } from './ZookeeperDetail';
 
 function Staff() {
   let zookeepers = useSelector((state: ZookeeperState) => state.zookeepers);
-  
+
   return (
     <View style={styles.viewContainer}>
-      <Title style={{flex: 1}} title="STAFF"></Title>
-      <View style={{flex: 5}}>
+      <Title style={{ flex: 1 }} title='STAFF'></Title>
+      <View style={{ flex: 5 }}>
         <FlatList
-            data={zookeepers}
-            renderItem={({ item }) => (
-              <ZookeeperDetail data={item}></ZookeeperDetail>
-            )}
-            keyExtractor={(item, index) => item.username + index.toString()}
-          />
+          data={zookeepers}
+          renderItem={({ item }) => (
+            <ZookeeperDetail data={item}></ZookeeperDetail>
+          )}
+          keyExtractor={(item, index) => item.username + index.toString()}
+        />
       </View>
     </View>
-    
   );
 }
 
